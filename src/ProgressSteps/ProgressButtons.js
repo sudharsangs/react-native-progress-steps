37c8678e4bc30521a899d2bedcaff3df52f75c97
sudharsangs <g.s.sudharsan99@gmail.com>
2023-11-18 18:39:20 +0530

    fix: adjust button positions

--- src/ProgressSteps/ProgressButtons.js
+++ src/ProgressSteps/ProgressButtons.js
@@ -3,9 +3,9 @@ import { View } from 'react-native';
 
 const ProgressButtons = props => (
   <View style={{ flexDirection: 'row', marginTop: 90 }}>
-    <View style={{ position: 'absolute', left: 60, bottom: 40 }}>{props.renderCancelButton()}</View>
-    <View style={{ position: 'absolute', left: 60, bottom: 40 }}>{props.renderPreviousButton()}</View>
-    <View style={{ position: 'absolute', right: 60, bottom: 40 }}>{props.renderNextButton()}</View>
+    <View style={{ position: 'absolute', left: 10, bottom: 30 }}>{props.renderCancelButton()}</View>
+    <View style={{ position: 'absolute', left: 10, bottom: 30 }}>{props.renderPreviousButton()}</View>
+    <View style={{ position: 'absolute', right: 10, bottom: 30 }}>{props.renderNextButton()}</View>
   </View>
 );
 
